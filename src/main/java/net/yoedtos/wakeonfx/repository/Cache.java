3f81e798d8adbe3b61ff10f7db4ce54cafe0ecc0
Edson Yoshimaru <yoedtos@hotmail.com>
2023-10-14 21:20:06 +0900

    feat: host repository

---
+++ src/main/java/net/yoedtos/wakeonfx/repository/Cache.java
@@ -0,0 +1,62 @@
+package net.yoedtos.wakeonfx.repository;
+
+import static net.yoedtos.wakeonfx.util.Constants.HOSTS_FILE;
+
+import net.yoedtos.wakeonfx.exceptions.FSException;
+import net.yoedtos.wakeonfx.model.Host;
+import net.yoedtos.wakeonfx.repository.fs.HostsFileHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+class Cache {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Cache.class);
+
+    private HostsFileHandler fileHandler;
+    public final List<Host> hosts;
+    private static Cache cache;
+
+    private Cache() throws FSException {
+        try {
+            fileHandler = new HostsFileHandler(Paths.get(HOSTS_FILE));
+            this.hosts = new ArrayList<>(fileHandler.read());
+        } catch (FSException e) {
+            LOGGER.error(e.getMessage());
+            throw new FSException(e.getMessage());
+        }
+    }
+
+    public void write() throws FSException {
+        try {
+            fileHandler.write(hosts);
+        } catch (FSException e) {
+            LOGGER.error(e.getMessage());
+            throw new FSException(e.getMessage());
+        }
+    }
+
+    public void update(List newHosts) throws FSException {
+        try {
+            fileHandler.write(newHosts);
+        } catch (FSException e) {
+            LOGGER.error(e.getMessage());
+            throw new FSException(e.getMessage());
+        }
+    }
+
+    public static synchronized Cache getInstance() {
+        if (cache == null) {
+            try {
+                cache = new Cache();
+                LOGGER.info("Created Cache id: {}", cache);
+            } catch (FSException e) {
+                LOGGER.error("Failed to create due to: {}", e.getMessage());
+                return null;
+            }
+        }
+        return cache;
+    }
+}
--- src/main/java/net/yoedtos/wakeonfx/repository/HostRepository.java
+++ src/main/java/net/yoedtos/wakeonfx/repository/HostRepository.java
@@ -1,34 +1,88 @@
 package net.yoedtos.wakeonfx.repository;
 
+import net.yoedtos.wakeonfx.exceptions.FSException;
 import net.yoedtos.wakeonfx.exceptions.RepositoryException;
 import net.yoedtos.wakeonfx.model.Host;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HostRepository implements Repository<Host> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HostRepository.class);
+
+    private Cache cache;
+    private List<Host> hosts;
+
+    public HostRepository() {
+            cache = Cache.getInstance();
+            hosts = cache.hosts;
+    }
 
     @Override
     public Object persist(Host host) throws RepositoryException {
-        return null;
+        int id;
+        try {
+            id = hosts.size();
+            hosts.add(id, host);
+            cache.write();
+        } catch (FSException e) {
+            LOGGER.error(e.getMessage());
+            throw new RepositoryException(e.getMessage());
+        }
+        return id;
     }
 
     @Override
     public void remove(int id) throws RepositoryException {
-
+        try {
+            hosts.set(id, null);
+            var newHosts = new ArrayList<>(hosts);
+            newHosts.remove(id);
+            cache.update(newHosts);
+        } catch (FSException e) {
+            LOGGER.error(e.getMessage());
+            throw new RepositoryException(e.getMessage());
+        }
     }
 
     @Override
     public Host findById(int id) throws RepositoryException {
-        return null;
+        try {
+            return hosts.get(id);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new RepositoryException(e.getMessage());
+        }
     }
 
     @Override
     public Object merge(Host host) throws RepositoryException {
-        return null;
+        int id = 0;
+        try {
+            for (int i = 0; i < hosts.size() ; i++) {
+                if (host.getName().equals(hosts.get(i).getName())) {
+                    id = i;
+                    hosts.set(i ,host);
+                    cache.write();
+                    break;
+                }
+            }
+        } catch (FSException e) {
+            LOGGER.error(e.getMessage());
+            throw new RepositoryException(e.getMessage());
+        }
+        return id;
     }
 
     @Override
     public List<Host> findAll() throws RepositoryException {
-        return null;
+        try {
+            return hosts;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new RepositoryException(e.getMessage());
+        }
     }
 }

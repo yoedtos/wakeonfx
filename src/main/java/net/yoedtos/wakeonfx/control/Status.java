b922ed7dd1933ab3bb2f0150cd7f9d0a07010f05
Edson Yoshimaru <yoedtos@hotmail.com>
2023-10-16 20:50:43 +0900

    feat: monitor service

---
+++ src/main/java/net/yoedtos/wakeonfx/control/Status.java
@@ -0,0 +1,7 @@
+package net.yoedtos.wakeonfx.control;
+
+public enum Status {
+    ON_LINE,
+    IS_ERROR,
+    OFF_LINE,
+}
---
+++ src/main/java/net/yoedtos/wakeonfx/service/MonitorService.java
@@ -0,0 +1,29 @@
+package net.yoedtos.wakeonfx.service;
+
+import net.yoedtos.wakeonfx.control.Status;
+import net.yoedtos.wakeonfx.core.net.NetMonitor;
+import net.yoedtos.wakeonfx.exceptions.NetworkException;
+import net.yoedtos.wakeonfx.exceptions.ServiceException;
+import net.yoedtos.wakeonfx.model.Host;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MonitorService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MonitorService.class);
+
+    public Status monitor(Host host) throws ServiceException {
+        NetMonitor netMonitor = new NetMonitor(host);
+        try {
+            if(!netMonitor.isHostOnline()) {
+                return Status.OFF_LINE;
+            }
+            if (!netMonitor.isHostServiceOnLine()) {
+                return Status.IS_ERROR;
+            }
+        } catch (NetworkException e) {
+            LOGGER.error(e.getMessage());
+            throw new ServiceException(e.getMessage());
+        }
+        return Status.ON_LINE;
+    }
+}

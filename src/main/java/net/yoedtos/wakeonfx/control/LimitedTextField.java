4f8a9ef60be10d4ffe053635d2c2f9eb3c262bbe
Edson Yoshimaru <yoedtos@hotmail.com>
2023-10-29 14:57:38 +0900

    feat: added text field character limitation

---
+++ src/main/java/net/yoedtos/wakeonfx/control/LimitedTextField.java
@@ -0,0 +1,36 @@
+package net.yoedtos.wakeonfx.control;
+
+import javafx.scene.control.TextField;
+
+public class LimitedTextField extends TextField {
+
+    private int limit;
+
+    @Override
+    public void replaceText(int start, int end, String text) {
+        if (validate())
+            super.replaceText(start, end, text);
+    }
+
+    @Override
+    public void replaceSelection(String replacement) {
+        if (validate())
+            super.replaceSelection(replacement);
+    }
+
+    private boolean validate() {
+        return lengthProperty().lessThan(limit).get();
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public int limitProperty() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+}

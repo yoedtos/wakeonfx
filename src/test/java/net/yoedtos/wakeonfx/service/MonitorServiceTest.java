955e9cddee1f85b89f98c3c77bd5382bfb88abd0
Edson Yoshimaru <yoedtos@hotmail.com>
2023-10-31 22:28:41 +0900

    test: added monitor with index method [monitor service]

--- src/test/java/net/yoedtos/wakeonfx/service/MonitorServiceTest.java
+++ src/test/java/net/yoedtos/wakeonfx/service/MonitorServiceTest.java
@@ -1,19 +1,34 @@
 package net.yoedtos.wakeonfx.service;
 
 import static net.yoedtos.wakeonfx.control.Status.*;
+import static net.yoedtos.wakeonfx.util.TestConstants.ID_ONE;
+import static net.yoedtos.wakeonfx.util.TestDataSet.createIndexOne;
 import static net.yoedtos.wakeonfx.util.TestDataSet.createSimpleHost;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mockConstruction;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import net.yoedtos.wakeonfx.core.net.NetMonitor;
+import net.yoedtos.wakeonfx.exceptions.RepositoryException;
 import net.yoedtos.wakeonfx.exceptions.ServiceException;
 import net.yoedtos.wakeonfx.model.Host;
+import net.yoedtos.wakeonfx.repository.HostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockedConstruction;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class MonitorServiceTest {
+
+    @Mock
+    private HostRepository mockHostRepo;
+    @Spy
+    @InjectMocks
+    private MonitorService monitorService;
 
     private Host host;
 
@@ -65,6 +80,13 @@ class MonitorServiceTest {
             assertThat(status).isEqualTo(OFF_LINE);
             assertThat(mocked.constructed()).hasSize(1);
         }
+    }
+
+    @Test
+    void whenMonitorWithIndexShouldCallRightHost() throws RepositoryException, ServiceException {
+        when(mockHostRepo.findById(ID_ONE)).thenReturn(host);
+        monitorService.monitor(createIndexOne());
+        verify(monitorService, times(1)).monitor(host);
     }
 }
 

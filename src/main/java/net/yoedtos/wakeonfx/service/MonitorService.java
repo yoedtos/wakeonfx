31a6825203de1a94c1dc5869459f13a0a20f830b
Edson Yoshimaru <yoedtos@hotmail.com>
2023-10-31 22:28:27 +0900

    feat: added monitor with index method [monitor service]

--- src/main/java/net/yoedtos/wakeonfx/service/MonitorService.java
+++ src/main/java/net/yoedtos/wakeonfx/service/MonitorService.java
@@ -4,16 +4,30 @@ import net.yoedtos.wakeonfx.control.Index;
 import net.yoedtos.wakeonfx.control.Status;
 import net.yoedtos.wakeonfx.core.net.NetMonitor;
 import net.yoedtos.wakeonfx.exceptions.NetworkException;
+import net.yoedtos.wakeonfx.exceptions.RepositoryException;
 import net.yoedtos.wakeonfx.exceptions.ServiceException;
 import net.yoedtos.wakeonfx.model.Host;
+import net.yoedtos.wakeonfx.repository.HostRepository;
+import net.yoedtos.wakeonfx.repository.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MonitorService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MonitorService.class);
 
+    private Repository<Host> repository;
+
+    public MonitorService() {
+        this.repository = new HostRepository();
+    }
     public Status monitor(Index index) throws ServiceException {
-        return null;
+        try {
+            var host = repository.findById(index.getId());
+            return monitor(host);
+        } catch (RepositoryException e) {
+            LOGGER.error(e.getMessage());
+            throw new ServiceException(e.getMessage());
+        }
     }
 
     public Status monitor(Host host) throws ServiceException {

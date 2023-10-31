1c54237e151bb28d94616edde6397451f5f0dc06
Edson Yoshimaru <yoedtos@hotmail.com>
2023-10-31 21:03:45 +0900

    feat: added wake with index method [wakeon service]

--- src/main/java/net/yoedtos/wakeonfx/service/WakeOnService.java
+++ src/main/java/net/yoedtos/wakeonfx/service/WakeOnService.java
@@ -6,19 +6,31 @@ import net.yoedtos.wakeonfx.core.net.INetHandler;
 import net.yoedtos.wakeonfx.core.net.LocalNetHandler;
 import net.yoedtos.wakeonfx.core.net.NetHandler;
 import net.yoedtos.wakeonfx.core.net.Packet;
-import net.yoedtos.wakeonfx.exceptions.CoreException;
-import net.yoedtos.wakeonfx.exceptions.NetworkException;
-import net.yoedtos.wakeonfx.exceptions.ServiceException;
-import net.yoedtos.wakeonfx.exceptions.ValidationException;
+import net.yoedtos.wakeonfx.exceptions.*;
 import net.yoedtos.wakeonfx.model.Address;
 import net.yoedtos.wakeonfx.model.Host;
+import net.yoedtos.wakeonfx.repository.HostRepository;
+import net.yoedtos.wakeonfx.repository.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WakeOnService {
     private static final Logger LOGGER = LoggerFactory.getLogger(WakeOnService.class);
 
+    private Repository<Host> repository;
+
+    public WakeOnService() {
+        this.repository = new HostRepository();
+    }
+
     public void wake(Index index) throws ServiceException {
+        try {
+            var host = repository.findById(index.getId());
+            wake(host);
+        } catch (RepositoryException e) {
+            LOGGER.error(e.getMessage());
+            throw new ServiceException(e.getMessage());
+        }
     }
 
     public void wake(Host host) throws ServiceException {

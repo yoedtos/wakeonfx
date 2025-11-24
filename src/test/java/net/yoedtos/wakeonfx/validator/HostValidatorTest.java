d2d29f9c9e96107670651c39935cd4a8f11380f4
Edson Yoshimaru <yoedtos@hotmail.com>
2025-11-25 02:12:58 +0900

    test: validate timeout - host validator

--- src/test/java/net/yoedtos/wakeonfx/validator/HostValidatorTest.java
+++ src/test/java/net/yoedtos/wakeonfx/validator/HostValidatorTest.java
@@ -29,22 +29,42 @@ class HostValidatorTest {
 
     @Test
     void whenHostNameIsInvalidShouldFail() {
-        Host nameInvalidHost = new Host("", PORT_NUM_ONE, null);
+        Host nameInvalidHost = new Host("", PORT_NUM_ONE, TIME_OUT_ONE, null);
         assertThatExceptionOfType(ValidationException.class)
                 .isThrownBy(() -> hostValidator.validate(nameInvalidHost));
     }
 
     @Test
     void whenHostPortIsInvalidShouldFail() {
-        Host portInvalidHost = new Host(SIMPLE_HOST, 0, null);
+        Host portInvalidHost = new Host(SIMPLE_HOST, 0, TIME_OUT_ONE, null);
+        assertThatExceptionOfType(ValidationException.class)
+                .isThrownBy(() -> hostValidator.validate(portInvalidHost));
+    }
+    @Test
+    void whenHostPortIsOutOfRangeShouldFail() {
+        Host portInvalidHost = new Host(SIMPLE_HOST, 66000, TIME_OUT_ONE, null);
         assertThatExceptionOfType(ValidationException.class)
                 .isThrownBy(() -> hostValidator.validate(portInvalidHost));
     }
 
     @Test
+    void whenHostTimeoutIsInvalidShouldFail() {
+        Host timeoutInvalidHost = new Host(SIMPLE_HOST, PORT_NUM_ONE, 0, null);
+        assertThatExceptionOfType(ValidationException.class)
+                .isThrownBy(() -> hostValidator.validate(timeoutInvalidHost));
+    }
+
+    @Test
+    void whenHostTimeoutIsOutOfRangeShouldFail() {
+        Host timeoutInvalidHost = new Host(SIMPLE_HOST, PORT_NUM_ONE, 301, null);
+        assertThatExceptionOfType(ValidationException.class)
+                .isThrownBy(() -> hostValidator.validate(timeoutInvalidHost));
+    }
+
+    @Test
     void whenHostIpAddressIsInvalidShouldFail() {
         Address ipInvalid = new Address(IP_ADD_BAD, null, null);
-        Host host = new Host(SIMPLE_HOST, PORT_NUM_ONE, ipInvalid);
+        Host host = new Host(SIMPLE_HOST, PORT_NUM_ONE, TIME_OUT_ONE, ipInvalid);
         assertThatExceptionOfType(ValidationException.class)
                 .isThrownBy(() -> hostValidator.validate(host));
     }
@@ -52,7 +72,7 @@ class HostValidatorTest {
     @Test
     void whenHostMacAddressIsInvalidShouldFail() {
         Address invalidMac = new Address(IP_ADD_ONE, BAD_HEXS, null);
-        Host host = new Host(SIMPLE_HOST, PORT_NUM_ONE, invalidMac);
+        Host host = new Host(SIMPLE_HOST, PORT_NUM_ONE, TIME_OUT_ONE, invalidMac);
         assertThatExceptionOfType(ValidationException.class)
                 .isThrownBy(() -> hostValidator.validate(host));
     }
@@ -60,7 +80,7 @@ class HostValidatorTest {
     @Test
     void whenHostSecureOnIsInvalidShouldFail() {
         Address invalidSecOn = new Address(IP_ADD_ONE, MAC_ADD_ONE, BAD_HEXS);
-        Host host = new Host(SIMPLE_HOST, PORT_NUM_ONE, invalidSecOn);
+        Host host = new Host(SIMPLE_HOST, PORT_NUM_ONE, TIME_OUT_ONE, invalidSecOn);
         assertThatExceptionOfType(ValidationException.class)
                 .isThrownBy(() -> hostValidator.validate(host));
     }
